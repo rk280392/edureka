1c82be76f7e2f7ef4ddd27553a1ddcc27ce00aae
rajesh kumar <rk90229@gmail.com>
2020-01-13 02:41:38 +0530

    testing

--- 1.py
+++ 1.py
@@ -8,9 +8,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_edureka():
-    service_log_path = "{}/firefox.log".format("/home/vagrant/jenkins/workspace/job5_5")
-    service_args = ['--verbose']
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", service_args=service_args,service_log_path=service_log_path)
+    #service_log_path = "{}/firefox.log".format("/home/vagrant/jenkins/workspace/job5_5")
+    #service_args = ['--verbose']
+    #driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", service_args=service_args,service_log_path=service_log_path)
+    driver = webdriver.Chrome()
     driver.get("http://192.168.0.106:5000/index.php")
     driver.maximize_window()
     link = driver.find_element_by_xpath("//a[@id='About Us']")
