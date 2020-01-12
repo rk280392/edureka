f29fb5e2ab7fcbd1850c8586a7f83f3c8fb7038e
rajesh kumar <rk90229@gmail.com>
2020-01-12 12:17:01 +0530

    removed unwanted from docker

--- 1.py
+++ 1.py
@@ -4,13 +4,14 @@ import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 
-
 def test_edureka():
-    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+    service_log_path = "{}/firefox.log".format("/home/rajesh-pc/study/edureka/projCert")
+    service_args = ['--verbose']
+    driver = webdriver.Firefox(executable_path="/usr/bin/firefox",service_args=service_args,service_log_path=service_log_path)
     driver.get("http://127.0.0.1:5000/index.php")
-    driver.maximize_window()
  #   link = driver.find_element_by_xpath("//a[@id='About Us']")
  #   link.click()
 
