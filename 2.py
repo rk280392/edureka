9be46f09cc8fcdfb3a4e3cf7dd306063035aeb3a
rajesh kumar <rk90229@gmail.com>
2020-01-13 01:44:00 +0530

    correct script for slave

--- 2.py
+++ 2.py
@@ -8,10 +8,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_edureka():
-    service_log_path = "{}/firefox.log".format("/home/vagrant/jenkins/workspace/job5_5")
+    service_log_path = "{}/firefox.log".format("/home/rajesh-pc/PycharmProjects/Edureka_project_1/testing1")
     service_args = ['--verbose']
     driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", service_args=service_args,service_log_path=service_log_path)
-    driver.get("http://192.168.0.106:5000/index.php")
+    driver.get("http://192.168.0.121:5000/index.php")
     driver.maximize_window()
     link = driver.find_element_by_xpath("//a[@id='About Us']")
     link.click()
