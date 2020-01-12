ea820f07efa66e3f1b15223e89c54be4c13284c3
rajesh kumar <rk90229@gmail.com>
2020-01-12 11:05:38 +0530

    changed chromedriver location

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_edureka():
-    driver = webdriver.Chrome(executable_path="/home/rajesh-pc/study/edureka/projCert/chromedriver_79/chromedriver")
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
     driver.get("http://127.0.0.1:5000/index.php")
     driver.maximize_window()
     link = driver.find_element_by_xpath("//a[@id='About Us']")
