8b46dc1577cb71e2fb4ec2f13a692057a9bbdbb7
rajesh kumar <rk90229@gmail.com>
2020-01-13 11:02:49 +0530

    testing

--- 1.py
+++ 1.py
@@ -11,7 +11,9 @@ def test_edureka():
     #service_log_path = "{}/firefox.log".format("/home/vagrant/jenkins/workspace/job5_5")
     #service_args = ['--verbose']
     #driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", service_args=service_args,service_log_path=service_log_path)
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("http://192.168.0.106:5000/index.php")
     driver.maximize_window()
     link = driver.find_element_by_xpath("//a[@id='About Us']")
