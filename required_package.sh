721e822a03094d3d7f816206e34901a61852671a
rajeshk <srk280392@gmail.com>
2020-01-17 01:07:52 +0530

    required packages

--- required_package.sh
+++ required_package.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
+sudo apt-get install python-pip
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
